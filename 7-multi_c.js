07d15f5560681dca9e2a0a71a9781589f4ba0807
Carolyne Yeko <yekocarol2@gmail.com>
2025-11-09 21:37:24 +0300

    Modified file 7

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,11 +1,12 @@
 const arg = process.argv[2];
 const x = parseInt(arg, 10);
 if (Number.isNaN(x)){
-    console.log("Missing number of occurrences");
+    console.log('Missing number of occurrences');
 
-} else{let i=0; while (i < x){
+} else {let i=0; while (i < x) {
     console.log('C is fun');
     i++;
+    
     }}
 
     
