496687cb03a354aac8c640a95e5f7870a12e11ab
Carolyne Yeko <yekocarol2@gmail.com>
2025-11-09 15:55:49 +0300

    Created first, second and thrid line

---
+++ 1-multi_languages.js
@@ -0,0 +1,11 @@
+let lineOne = "C is fun";
+let lineTwo = "Python is cool";
+let lineThree = "JavaScript is amazing";
+console.log(lineOne);
+console.log(lineTwo);
+console.log(lineThree);
+// output
+//$ node 1-multi_languages.js
+//C is fun
+//Python is cool
+//JavaScript is amazing
